7b42fe08b273ab32e84904cda29153de0c731ebc
GreegAV <greegav@gmail.com>
2018-01-23 22:33:39 +0200

    Connection pool, tests, datasource

--- src/main/java/commands/LogoutCommand.java
+++ src/main/java/commands/LogoutCommand.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
 
-
 public class LogoutCommand implements ICommand {
 
     private static Logger logger = org.apache.log4j.Logger.getLogger(LogoutCommand.class);
